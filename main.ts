a804be992eecbb5540b491666d9cefb725fe1326
saahil1111 <122548152+saahil1111@users.noreply.github.com>
2023-01-19 11:24:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    sprite = game.createSprite(2, 2)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (true) {
+    	
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("Hello! press A+B")
+})
+let sprite: game.LedSprite = null
+for (let index = 0; index < 1; index++) {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
