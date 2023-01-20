7fadb42206fae1285fa383dcf36c615ce9dbf3fd
saahil1111 <122548152+saahil1111@users.noreply.github.com>
2023-01-20 11:17:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    sprite = 0
+})
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
@@ -5,27 +8,44 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
 })
 input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 10; index++) {
-        basic.showString("A", 5)
-basic.showString("B", 5)
-basic.showString("C", 5)
-basic.showString("D", 5)
+    for (let index = 0; index < 1; index++) {
+        basic.showString("1", 5)
+basic.showString("2", 5)
+basic.showString("3", 5)
+basic.showString("4", 5)
+basic.showString("5", 5)
+basic.showString("6", 5)
     }
     Letter = randint(1, 4)
     if (Letter == 1) {
-        basic.showString("A")
+        basic.showString("1")
     } else if (Letter == 2) {
-        basic.showString("B")
+        basic.showString("2")
     } else if (Letter == 3) {
-        basic.showString("C")
+        basic.showString("3")
+    } else if (Letter == 4) {
+        basic.showString("4")
+    } else if (Letter == 5) {
+        basic.showString("5")
     } else {
-        basic.showString("D")
+        basic.showString("6")
     }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    basic.showString("Hello! press A+B")
+    music.playMelody("- - - - - - - - ", 120)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
 })
 let Letter = 0
+let sprite = 0
 for (let index = 0; index < 1; index++) {
     basic.showLeds(`
         # # # # #
