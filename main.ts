0318af62c9caa1aa57ef6faf36a669deb4284826
saahil1111 <122548152+saahil1111@users.noreply.github.com>
2023-01-20 11:05:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,31 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
-input.onButtonPressed(Button.AB, function () {
-    sprite = game.createSprite(2, 2)
-})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showArrow(ArrowNames.North)
-})
-input.onGesture(Gesture.TiltRight, function () {
-    if (true) {
-        sprite.turn(Direction.Right, 45)
-        sprite.move(1)
+    for (let index = 0; index < 10; index++) {
+        basic.showString("A", 5)
+basic.showString("B", 5)
+basic.showString("C", 5)
+basic.showString("D", 5)
+    }
+    Letter = randint(1, 4)
+    if (Letter == 1) {
+        basic.showString("A")
+    } else if (Letter == 2) {
+        basic.showString("B")
+    } else if (Letter == 3) {
+        basic.showString("C")
+    } else {
+        basic.showString("D")
     }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.showString("Hello! press A+B")
 })
-let sprite: game.LedSprite = null
+let Letter = 0
 for (let index = 0; index < 1; index++) {
     basic.showLeds(`
         # # # # #
